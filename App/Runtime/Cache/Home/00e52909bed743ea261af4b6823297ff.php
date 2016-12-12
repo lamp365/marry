804dc4e72309e09fd8a8d3f4e969dfb6f340c2d7
kevin.liu <791845283@qq.com>
2016-12-12 23:48:51 +0800

    add

---
+++ App/Runtime/Cache/Home/00e52909bed743ea261af4b6823297ff.php
@@ -0,0 +1,19 @@
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="/marry/Public/Home/css/bootstrap.min.css">
+    <script src="/marry/Public/Home/js/jquery-1.10.2.min.js"></script>
+    <script src="/marry/Public/Home/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="row">
+    <div class="col-xs-4">.col-xs-12 .col-md-8</div>
+    <div class="col-xs-8">.col-xs-6 .col-md-4</div>
+</div>
+</body>
+</html>
